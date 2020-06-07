3e5e6e7d7f87a9edb47e0814742f1a698c94ef93
tobeheard <sharyn.brand@gmail.com>
2020-06-07 16:47:30 +1000

    stage 1

--- Painting_step_1/pixelPaint1/sketch.js
+++ Painting_step_1/pixelPaint1/sketch.js
@@ -1,7 +1,18 @@
+// painting with particle pixels
+let video;
+let vScale = 16;
+let particle;
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	pixelDensity(1);
+	video = createCapture(VIDEO);
+	video.size(width / vScale, height / vScale);
+	particle = new Particle(320, 240);
 }
 
 function draw() {
-
+	video.loadPixels();
+	particle.update();
+	particle.show();
 }
