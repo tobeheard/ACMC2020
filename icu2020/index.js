21d8cdf6391dc061a093d4421201ccbbb3c9691f
tobeheard <sharyn.brand@gmail.com>
2020-06-21 23:49:05 +1000

    goodNew

---
+++ icu2020/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const Datastore = require('nedb');
+
+const app = express();
+app.listen(3000, () => console.log('hello, welcome to the installation'));
+app.use(express.static('public'));
+app.use(express.json({
+  limit: '1mb'
+}));
+
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+const allData = [];
+
+app.post('/api', (request, response) => {
+  console.log('I got a request!');
+  const data = request.body;
+  const timestamp = Date.now();
+  data.timestamp = timestamp;
+  database.insert(data);
+  response.json(data);
+});
